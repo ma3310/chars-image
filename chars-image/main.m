c1e0d7e2495fd158c3ff7d1235761683b17862a3
Jun Ni <jun.ni@cndevel.com>
2015-07-10 00:36:48 +0800

    Initial Commit

---
+++ chars-image/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chars-image
+//
+//  Created by Jun Ni on 7/10/15.
+//  Copyright (c) 2015 Jun Ni. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
